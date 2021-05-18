26e529bbb0688cbd23d335727930aceee288149e
kdi19u788 <dkostev68@gmail.com>
2021-05-18 19:32:29 +0300

    delete waste

--- 3 semester/Data types and structures/lab_03/src/main.c
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-#include "utils.h"
-
-int main() {
-  setbuf(stdout, NULL);
-  setbuf(stdin, NULL);
-  print_menu();
-}
--- 3 semester/Data types and structures/lab_05/inc/utils.h
+++
@@ -1,7 +0,0 @@
-
-double random_double(const double bottom, const double top);
-
-int wait_and_clear();
-int clear_input();
-
-void print_menu();
