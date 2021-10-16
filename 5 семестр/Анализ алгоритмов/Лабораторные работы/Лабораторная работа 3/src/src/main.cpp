cd0d7669550b4fed26b431e7a551a092e7e1a76d
kdi19u788 <dkostev68@gmail.com>
2021-10-16 11:43:30 +0300

    Update Lab_3

--- 5 семестр/Анализ алгоритмов/Лабораторные работы/Лабораторная работа 3/src/src/main.cpp
+++ 5 семестр/Анализ алгоритмов/Лабораторные работы/Лабораторная работа 3/src/src/main.cpp
@@ -33,8 +33,8 @@ double checkTime(Array<Type> &a, int (*cmp)(Type, Type), void (Array<Type>::*sor
     for (int i = 0; i < 100; i++) {
         for (size_t i = 0; i < a.getLength(); i++)
             // a[i] = i;
-            //a[i] = a.getLength() - i; 
-            a[i] = rand() % 1000 - 500;
+            a[i] = a.getLength() - i; 
+            // a[i] = rand() % 1000 - 500;
         gettimeofday(&tv_start, NULL);
         (a.*sort)(cmp);
         gettimeofday(&tv_stop, NULL);
