005f14d7b8569dc1bd98d61706f83ec294be76d3
kdi19u788 <dkostev68@gmail.com>
2021-12-23 08:39:12 +0300

    Fix lab_3

--- 5 семестр/Базы Данных/Лабораторные работы/Лабораторная работа 3/src/05_procedure.sql
+++ 5 семестр/Базы Данных/Лабораторные работы/Лабораторная работа 3/src/05_procedure.sql
@@ -3,18 +3,17 @@
 -- Добавляет юриста
 
 
-CREATE OR REPLACE PROCEDURE insert_lawyer (id INT, 
-                                           name VARCHAR, 
-                                           email_address VARCHAR, 
-                                           phone_number VARCHAR, 
-                                           skill INT, 
-                                           office_address VARCHAR, 
-                                           process_count INT)
+CREATE OR REPLACE PROCEDURE insert_lawyer (u_name VARCHAR, 
+                                           u_email_address VARCHAR, 
+                                           u_phone_number VARCHAR, 
+                                           u_skill INT, 
+                                           u_office_address VARCHAR, 
+                                           u_process_count INT)
 AS 
 $$
 BEGIN
-    INSERT INTO lawyers 
-    VALUES (id, name, email_address, phone_number, skill, office_address, process_count);
+    INSERT INTO lawyers (name, email_address, phone_number, skill, office_address, process_count)
+    VALUES (u_name, u_email_address, u_phone_number, u_skill, u_office_address, u_process_count);
 END;
 $$
 LANGUAGE plpgsql;
